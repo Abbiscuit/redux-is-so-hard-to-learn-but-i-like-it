80cd28e3e456a2a922d0c0d5dd1c592475ea6fa4
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2019-08-14 22:19:50 +0900

    Add article-counter component just for another demonstaration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import List from './components/list.component';
+import Counter from './components/counter.component';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h2>Articles</h2>
+        <Counter />
         <List />
       </div>
     );
---
+++ src/components/counter.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { connect } from 'react-redux';
+
+const Counter = ({ articles }) => {
+  return (
+    <section className="article-counter">
+      <span>{`${articles.length} articles now`}</span>
+    </section>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    articles: state.articles.articles
+  };
+};
+
+export default connect(mapStateToProps)(Counter);
